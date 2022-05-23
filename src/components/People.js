30d0e2cdab36af26115695ec8a13e7cfc9f3e6d3
gyawali9 <roshangyawali9@gmail.com>
2022-05-23 16:20:21 +0545

    removing unwanted codes

--- src/components/People.js
+++ src/components/People.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { v4 as uuidv4 } from 'uuid';
 import { usePeoplesStore } from '../store';
-import useStore from '../store'
 
 const People = () => {
   const people = usePeoplesStore(state => state.people)
